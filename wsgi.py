86cb55afee182adff296a69ef682e9420036e77c
Tanner Varrelman <tanner@nkn-ui-web-dev3.nkn.uidaho.edu>
2020-11-24 12:17:28 -0800

    added virtual env info for mod_wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,10 +1,24 @@
 """
 wsgi.py imports and starts our entire app
 """
+# Path to the virtual env associated with this app
+python_home = '/nethome/tanner/TannersApp/LassaVirtualEnv/'
+
+import sys
+import site
+
+# Calculate path to site-packages directory.
+
+python_version = '.'.join(map(str, sys.version_info[:2]))
+site_packages = python_home + '/lib/python%s/site-packages' % python_version
+
+# Add the site-packages directory.
+
+site.addsitedir(site_packages)
+
 
 # Import our create_app function from our package
 from LassaMappingApp import create_app
-
 
 app = create_app()
 
